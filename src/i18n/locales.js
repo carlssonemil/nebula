df713a68d27a1e1df8b28c8c1085ff65588f9b0f
zvbt <cyadine@pm.me>
2024-12-14 10:40:23 +0100

    Add French (fr-FR) translations to locale files

--- src/i18n/locales.js
+++ src/i18n/locales.js
@@ -1,14 +1,17 @@
 import enUS from './locales/en-US.json'
 import arSA from './locales/ar-SA.json'
 import ptBR from './locales/pt-BR.json'
+import frFR from './locales/fr-FR.json'
 
 export const translatedBy = {
   'ar-SA': { github: 'https://github.com/mo9a7i', name: 'Mohannad Otaibi' },
   'pt-BR': { github: 'https://github.com/victorrib01', name: 'Victor Ribeiro' },
+  'fr-FR': { github: 'https://github.com/zvbt', name: 'Cyadine' },
 }
 
 export default {
   'en-US': enUS,
   'ar-SA': arSA,
   'pt-BR': ptBR,
+  'fr-FR': frFR,
 }
